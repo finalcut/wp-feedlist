c0884d27ea938b2a145d94c99c099bebc7b01298
bill.rawlinson <bill.rawlinson@bebd74ea-a419-0410-8ca7-71696fc22498>
2006-08-18 12:30:46 +0000

    added ability to show a subset of your thumbnails and to randomize those that are shown.

--- trunk/picasaWeb/picasaWeb.php
+++ trunk/picasaWeb/picasaWeb.php
@@ -23,11 +23,58 @@ if (file_exists(dirname(__FILE__).'/../../wp-includes/rss-functions.php')) {
 
 	
 	
-	function picasaWeb($url){
+	function picasaWeb($args){
+
+		$p=array();
+		$p['url']="";
+		$p['random']=false;
+		$p['num']=0;
+
+
+		if (is_array($args)){
+			if(!isset($args['url'])){
+				print "No URL Provided";
+				return false;
+			}else{
+				$p['url'] = $args['url'];
+			}
+
+
+			if (isset($args['random'])){
+
+					if ($args['random'] == 'true' || $args['random'] == 1){
+						$p['random'] = true;
+					}else{
+						$p['random'] = false;
+					}
+
+			}
+			
+			if (isset($args['num']) && is_numeric($args['num']))
+			{
+				$p['num'] = $args['num'];
+			}
+
+		} else {
+			$p['url'] = $args;
+		}
+
+
+
 		$list = "";
 		
-		if($images = fetch_rss($url)){
+		if($images = fetch_rss($p['url'])){
 			$images = $images->items;
+			if ($p['random'])
+			{
+				// We want a random selection, so lets shuffle it
+				shuffle($images);
+			}
+			if ($p['num'] > 0)
+			{
+				// Slice off the number of items that we want:
+				$images = array_slice($images, 0, $p['num']);
+			}
 
 
 			foreach ($images as $image) {
