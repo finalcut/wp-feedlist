4d677d279d00eb4069e24ef21c6a9e5e576db142
bill.rawlinson <bill.rawlinson@bebd74ea-a419-0410-8ca7-71696fc22498>
2007-02-26 18:51:53 +0000

    provide additional argument options

--- trunk/topCategories/top_categories.php
+++ trunk/topCategories/top_categories.php
@@ -3,21 +3,32 @@
 Plugin Name: Top Categories
 Plugin URI: http://rawlinson.us/blog/articles/top-categories-plugin/
 Description: Display your top x categories by post where x is the number of categories you want to display.  Can be seen in use at: <a href="http://rawlinson.us/blog/archives/">Top Categories</a>.
-Version: 1.0
+Version: 1.1
 Author: Bill Rawlinson
 Author URI: http://blog.rawlinson.us/
 */
-function top_categories($displaynum=10)
+function top_categories($args = '')
 {
 
+		if( is_numeric($args){ // in case user just passed in the number of categories to show (maintains backwards compatability)
+			$r = array('number'=>$args);
+		} else if ( is_array($args) ){ // if user defines the array to pass in (use $defaults for information the structure the array must match)
+			$r = &$args;
+		} else{
+			parse_str($args, $r); // in case they pass in a "Url" type string of arguments
+		}
+
+
+		// $exclude and $include are comma separated lists of category ids such as '1,2,3,4'
 
 		$defaults = array('type' => 'post', 'child_of' => 0, 'orderby' => 'count', 'order' => 'DESC',
 		'hide_empty' => false, 'include_last_update_time' => false, 'hierarchical' => 0, $exclude => '', $include => '',
-		'number' => $displaynum);
+		'number' => 10);
+
+		$r = array_merge($defaults, $r);
 
 
-
-		$cats = get_categories($defaults);
+		$cats = get_categories($r);
 
 
 		$catcount = 0;
@@ -29,29 +40,6 @@ function top_categories($displaynum=10)
         }
 
 
-		// order from lowest to highest
-		//natsort($counts);
-
-
-		// get the last ten,which have the highest counts ..
-		//$counts = array_slice($counts,$displaynum,$displaynum);
-
-		// reverse the order so we go from highest to lowest
-		//$counts = array_reverse($counts);
-
-/*
-		print "<ul>";
-        foreach ($counts as $catname => $count)
-        {
-                $catlink = $catlinks{$catname};
-                if (strstr($catlink, "http:") == FALSE) {
-                        print "<li><a href=\"$myurl$category_base/$catlink\" rel=\"tag\" title=\"$count entries\">$catname</a></li>";
-                } else {
-                        print "<li><a href=\"$catlink\" rel=\"tag\" title=\"$count entries\">$catname</a></li>";
-                }
-        }
-		print "</ul>";
-	*/
 	print $output;
 }
 ?>
